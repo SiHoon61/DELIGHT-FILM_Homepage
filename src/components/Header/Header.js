f791fdfcc648097498683a1bd10d075c4f2c5594
SiHoon61 <dlatlgns000@gmail.com>
2024-06-26 17:42:54 +0900

    Create repository

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    Home a
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Works
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default Header;
